7842c644507cae55785bc2af47e2acdae827bf1b
Jason Michaels <jason.michaels721@gmail.com>
2019-07-13 10:47:34 +0000

    updates

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-from vibora import Vibora, JsonResponse
+from vibora import Vibora, Response
 
 app = Vibora()
 
 
 @app.route('/')
 async def home():
-    return JsonResponse({'hello': 'world'})
+    return Response(b'{"hello": "world"}', headers={'content-type': 'application/json'})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000)
